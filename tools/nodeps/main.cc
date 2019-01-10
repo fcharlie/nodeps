cc1cce3cdc4ff793d13dbde58a85b97c727f01e0
Force Charlie <charlieio@outlook.com>
2019-01-10 22:22:50 +0800

    Resolve All depends

--- tools/nodeps/main.cc
+++ tools/nodeps/main.cc
@@ -1,6 +1,99 @@
 //////////
+#include "pe.hpp"
+#include <filesystem>
+#include <unordered_set>
+
+using dllset_t = std::unordered_set<std::wstring>;
+
+class DepthAuxiliary {
+public:
+  DepthAuxiliary(int &i) : depth(i) { depth++; }
+  ~DepthAuxiliary() { depth--; }
+
+private:
+  int &depth;
+};
+
+class NoDependsEngine {
+public:
+  NoDependsEngine() = default;
+  bool Initialize(std::wstring_view file) {
+    std::error_code ec;
+    exefile = std::filesystem::canonical(file, ec);
+    if (ec || !std::filesystem::exists(exefile, ec)) {
+      fprintf(stderr, "File: %s\n", ec.message().c_str());
+      return false;
+    }
+    dir = exefile.parent_path();
+    return true;
+  }
+  bool Parse() { return ParsePE(exefile.wstring()); }
+  const dllset_t &Dlls() const { return ds; }
+  bool CopyToDist();
+
+private:
+  std::filesystem::path exefile;
+  std::filesystem::path dir;
+  dllset_t ds;
+  bool ParsePE(std::wstring_view file);
+  bool IsDllExistsUnrecorded(const std::wstring &dll, std::wstring &dp);
+  int depth{0};
+};
+
+bool NoDependsEngine::IsDllExistsUnrecorded(const std::wstring &dll,
+                                            std::wstring &dp) {
+  auto it = ds.find(dll);
+  if (it != ds.end()) {
+    /// is record
+    return false;
+  }
+  std::error_code ec;
+  auto dllpath = dir / dll;
+  if (std::filesystem::exists(dllpath, ec)) {
+    dp = dllpath.wstring();
+    return true;
+  }
+  return false;
+}
+
+bool NoDependsEngine::ParsePE(std::wstring_view pefile) {
+  base::error_code ec;
+  auto pm = pecoff::inquisitive_pecoff(pefile, ec);
+  DepthAuxiliary da(depth);
+  if (depth >= 2000) {
+    return false;
+  }
+  if (ec) {
+    return false;
+  }
+
+  for (const auto &e : pm->depends) {
+    std::wstring dll;
+    if (IsDllExistsUnrecorded(e, dll)) {
+      if (!ParsePE(dll)) {
+        return false;
+      }
+      ds.insert(dll);
+    }
+  }
+  return true;
+}
 
 int wmain(int argc, wchar_t **argv) {
-  //
+  if (argc < 2) {
+    wprintf(L"usage: %s pe\n", argv[0]);
+    return 1;
+  }
+  NoDependsEngine nd;
+  if (!nd.Initialize(argv[1])) {
+    return 1;
+  }
+  if (!nd.Parse()) {
+    return 1;
+  }
+  wprintf(L"Exe: %s depends:\n", argv[1]);
+  for (const auto &e : nd.Dlls()) {
+    wprintf(L"     %s\n", e.c_str());
+  }
   return 0;
 }
